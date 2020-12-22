1a0f1930488257d9beced0468a2607b97a906af7
James Luo <12567921+jluo117@users.noreply.github.com>
2020-12-22 12:55:47 -0800

    Time: 2700 ms (84.75%), Space: 296.9 MB (5.67%) - LeetHub

---
+++ ones-and-zeroes/ones-and-zeroes.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        strTable = []
+        for s in strs:
+            table = {'0':0,'1':0}
+            for c in s:
+                table[c] += 1
+            strTable.append(table)
+        dp = {}
+        def helper(index,m,n):
+            if index == len(strTable):
+                return 0
+            if (index,m,n) in dp:
+                return dp[index,m,n]
+            curTable = strTable[index]
+            res = 0
+            if curTable['0'] <= m and curTable['1'] <= n:
+                res = helper(index+1,m-curTable['0'],n-curTable['1'])+1
+            res = max(res,helper(index+1,m,n))
+            dp[(index,m,n)] = res
+            return res
+        return helper(0,m,n)
