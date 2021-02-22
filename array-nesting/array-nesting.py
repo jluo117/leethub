a8f250e514382b7d110cf5c15da3999f4f200bec
James Luo <12567921+jluo117@users.noreply.github.com>
2021-02-22 12:43:44 -0800

    Time: 144 ms (23.62%), Space: 35.6 MB (5.12%) - LeetHub

--- array-nesting/array-nesting.py
+++ array-nesting/array-nesting.py
@@ -1,13 +1,14 @@
 class Solution:
-    def arrayNesting(self, nums: List[int]) -> int:
-        visitSet = set()
-        def helper(index):
-            if index in visitSet or index >= len(nums):
-                return 0
-            visitSet.add(index)
-            res = helper(nums[index])+1
-            return res
-        maxVal = 0
-        for index in range(len(nums)):
-            maxVal = max(maxVal,helper(index))
-        return maxVal
+    def arrayNesting(self, nums: List[int]) -> int:
+        seenVal = set()
+        def helper(index):
+            if index in seenVal or index >= len(nums):
+                return 0
+            seenVal.add(index)
+            res = helper(nums[index])+1
+            
+            return res
+        maxVal = 0
+        for index in range(len(nums)):
+            maxVal = max(maxVal,helper(index))
+        return maxVal
