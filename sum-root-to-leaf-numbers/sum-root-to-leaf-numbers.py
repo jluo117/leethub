c46c24a139b7186129ad8b1715b2a74736eb7e18
James Luo <12567921+jluo117@users.noreply.github.com>
2021-03-01 12:57:10 -0800

    Time: 28 ms (89.22%), Space: 13.9 MB (100.00%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,22 +1,22 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def sumNumbers(self, root: TreeNode) -> int:
-        self.runningSum = 0
-        def helper(curStr,root):
-            if root.left == None and root.right == None:
-                curStr += str(root.val)
-                self.runningSum += int(curStr)
-                return
-            if root.left:
-                helper(curStr+str(root.val),root.left)
-            if root.right:
-                helper(curStr+str(root.val),root.right)
-        if root == None:
-            return 0
-        helper("",root)
-        return self.runningSum
+    def sumNumbers(self, root: TreeNode) -> int:
+        self.runningSum = 0
+        def helper(curStr,root):
+            if root.left == None and root.right == None:
+                curStr += str(root.val)
+                self.runningSum += int(curStr)
+                return
+            if root.left:
+                helper(curStr+str(root.val),root.left)
+            if root.right:
+                helper(curStr+str(root.val),root.right)
+        if root == None:
+            return 0
+        helper("",root)
+        return self.runningSum
