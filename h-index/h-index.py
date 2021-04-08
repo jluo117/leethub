d5d14c5b9c94ac82a700aef07ad9b4410bd7d2d9
James Luo <12567921+jluo117@users.noreply.github.com>
2021-04-08 12:18:20 -0700

    Time: 36 ms (73.29%), Space: 14.5 MB (68.07%) - LeetHub

--- h-index/h-index.py
+++ h-index/h-index.py
@@ -1,16 +1,30 @@
+'''
+0 1 3 5 6
+1
+2
+3
+'''
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        table = Counter()
+        citationCount = collections.Counter()
+        count = 0
         for cite in citations:
             if cite > len(citations):
-                table[len(citations)] += 1
+                citationCount[len(citations)] += 1
             else:
-                table[cite] += 1
-        running = 0
+                citationCount[cite] += 1
         
-        for i in range(len(citations),0,-1):
-            if table[i] + running  >= i:
-                return i
-            running += table[i]
+        running = 0
+        for index in range(len(citations),-1,-1):
+            if index in citationCount:
+                running += citationCount[index]
+            if running >= index:
+                return index
         return 0
+            
+        
+            
+        
+        
+        
         
