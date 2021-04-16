7da7e1b98e56675594106e97f8506991232aebc8
James Luo <12567921+jluo117@users.noreply.github.com>
2021-04-16 11:39:20 -0700

    Time: 512 ms (60.82%), Space: 18.6 MB (80.58%) - LeetHub

--- daily-temperatures/daily-temperatures.py
+++ daily-temperatures/daily-temperatures.py
@@ -1,16 +1,11 @@
-'''
-
-'''
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
-        res = [0]* len(T)
         stk = []
+        res = [0] * len(T)
         for index in range(len(T)-1,-1,-1):
-            while len(stk) and stk[-1][0] <= T[index]:
+            while stk and stk[-1][0] <= T[index]:
                 stk.pop()
             if len(stk):
                 res[index] = stk[-1][1] - index
             stk.append((T[index],index))
         return res
-        
-        
