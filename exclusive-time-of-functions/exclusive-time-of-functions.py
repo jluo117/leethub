afae3cad226c9f202ebb44e02a78da8a47bbedfd
James Luo <12567921+jluo117@users.noreply.github.com>
2021-06-16 14:58:42 -0700

    Time: 88 ms (22.56%), Space: 15.2 MB (5.85%) - LeetHub

--- exclusive-time-of-functions/exclusive-time-of-functions.py
+++ exclusive-time-of-functions/exclusive-time-of-functions.py
@@ -1,37 +1,32 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         self.index = 0
-        self.curTime = 0
         table = {}
         def helper():
-            tolRunTime = 0
-            logSplit = logs[self.index].split(':')
-            function = int(logSplit[0])
-            startTime = int(logSplit[2])
+            curId = int(logs[self.index].split(":")[0])
+            if self.index == len(logs):
+                return
+            runtime = 0
+            startTime = int(logs[self.index].split(":")[2])
             self.index += 1
-                
             while self.index < len(logs):
-                curLog = logs[self.index].split(':')
-                if curLog[1] == "start":
-                    tolRunTime += (int(curLog[2])-startTime)
+                if logs[self.index].split(":")[1] == "start":
+                    runtime += int(logs[self.index].split(":")[2]) - startTime
                     helper()
-                    startTime = int(logs[self.index].split(':')[2])+1
+                    startTime = int(logs[self.index].split(":")[2])+1
                     self.index += 1
-                    continue
-                if curLog[1] == "end":
-                    tolRunTime += (int(curLog[2])-startTime)+1
-                    
-                    if function in table:
-                        table[function] += tolRunTime
+                else:
+                    runtime += int(logs[self.index].split(":")[2]) - startTime+1
+                    if curId in table:
+                        table[curId] += runtime
                     else:
-                        table[function] = tolRunTime
-                    return 
+                        table[curId] = runtime
+                    return
         while self.index < len(logs):
             helper()
-            self.index += 1
-        ary = []
-        print(table)
-        for v in range(n):
-            ary.append(table[v])
-        return ary
-        
+        res = []
+        for i in range(n):
+            res.append(table[i])
+        return res
+            
+                
