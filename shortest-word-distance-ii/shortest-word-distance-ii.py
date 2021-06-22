7e643eecc4c74855ada428013f60a83fa6606a47
James Luo <12567921+jluo117@users.noreply.github.com>
2021-06-22 12:54:08 -0700

    Time: 96 ms (78.67%), Space: 22 MB (18.99%) - LeetHub

--- shortest-word-distance-ii/shortest-word-distance-ii.py
+++ shortest-word-distance-ii/shortest-word-distance-ii.py
@@ -1,27 +1,30 @@
 class WordDistance:
 
-    def __init__(self, wordsDict: List[str]):
-        self.graph = {}
-        for index in range(len(wordsDict)):
-            if wordsDict[index] not in self.graph:
-                self.graph[wordsDict[index]] = [index]
+    def __init__(self, words: List[str]):
+        self.table = {}
+        for index in range(len(words)):
+            if words[index] in self.table:
+                self.table[words[index]].append(index)
             else:
-                self.graph[wordsDict[index]].append(index)
-            
+                self.table[words[index]] = [index]
         
-                    
+        
 
     def shortest(self, word1: str, word2: str) -> int:
-        word1Index = self.graph[word1]
-        word2Index = self.graph[word2]
-        minVal = float('inf')
-        for val in word1Index:
-            for lowerVal in word2Index:
-                minVal =min(minVal,abs(val-lowerVal))
-        return minVal
-        
+        word1Table = self.table[word1]
+        word2Table = self.table[word2]
+        w1Index = 0
+        w2Index = 0
+        minDif = float('inf')
+        while w1Index < len(word1Table) and w2Index < len(word2Table):
+            minDif = min(minDif,abs(word1Table[w1Index]-word2Table[w2Index]))
+            if word1Table[w1Index] < word2Table[w2Index]:
+                w1Index += 1
+            else:
+                w2Index += 1
+        return minDif
 
 
 # Your WordDistance object will be instantiated and called as such:
-# obj = WordDistance(wordsDict)
+# obj = WordDistance(words)
 # param_1 = obj.shortest(word1,word2)
