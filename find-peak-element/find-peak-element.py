54614afabfeeee09dcea3468b7166e8fa87d6854
James Luo <12567921+jluo117@users.noreply.github.com>
2021-11-04 14:55:25 -0700

    Time: 48 ms (59.58%), Space: 14.5 MB (43.81%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,18 +1,19 @@
-'''
-[1,2,1,3,5,6,4]
-
-5 < 6
-6
-'''
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        low = 0
-        high = len(nums)-1
-        while low < high:
-            mid = (low+high)//2
-            if nums[mid] > nums[mid +1]:
-                high = mid
+        def helper(L,R):
+            if L == R:
+                return R
+            mid_point = (L+R)//2
+            if L + 1 == R:
+                if nums[L] > nums[R]:
+                    return L
+                else:
+                    return R
+            if nums[mid_point-1] < nums[mid_point] > nums[mid_point+1]:
+                return mid_point
+            if nums[mid_point] < nums[mid_point+1]:
+                return helper(mid_point+1,R)
             else:
-                low = mid + 1
-            
-        return low
+                return helper(L,mid_point-1)
+        return helper(0,len(nums)-1)
+                
